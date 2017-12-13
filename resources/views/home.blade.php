413147fcb4769c540a3f20e1ef66903ee29feb6a
akhil <b2akhilmj@gmail.com>
2017-12-13 20:20:25 +0530

    front end

---
+++ resources/views/home.blade.php
@@ -0,0 +1,311 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- SEO -->
+    <meta name="author" content="Surjith S M">
+    <meta name="description" content="MentalPress is a Responsive Medical and Health HTML5 Template ">
+    <meta name="keywords" content="health, medical, psychotherapy, website, template">
+
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+
+    <!-- Page Title -->
+    <title>HLIVE</title>
+
+    <!-- Main CSS -->
+    <link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
+
+    <!-- RTL : Enable RTL Style -->
+    <!-- <link href="assets/css/rtl.css" rel="stylesheet"> -->
+
+    <!-- Theme CSS -->
+    <!-- <link href="assets/css/themes/purple.css" rel="stylesheet"> -->
+
+    <!-- Modernizr -->
+    <script src="{{URL::asset('assets/js/modernizr.js')}}"></script>
+
+</head>
+
+<body>
+
+    <!-- ============================
+      TOP BAR
+     ============================ -->
+
+    <div class="top">
+        <div class="top__background"></div>
+        <div class="container">
+           <br>
+        </div>
+    </div>
+
+    <!-- ========  // END TOP BAR ======== -->
+
+    <!-- ============================
+      HEADER
+     ============================ -->
+
+    <div class="container">
+        <header class="header">
+            <div class="logo">
+                <a href="./index.html"> <img src="{{URL::asset('assets/images/logo.png')}}" alt="" width="160px"> </a>
+            </div>
+            <a href="#main-navigation" class="navbar-toggle"> <span class="navbar-toggle__text">MENU</span> <span class="navbar-toggle__icon-bar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </span>
+            </a>
+            <div class="header-widgets">
+               <!--  <div class="widget  header-widgets__widget  widget-icon-box">
+                    <div class="icon-box"><i class="fa  fa-home  fa-3x"></i>
+                        <div class="icon-box__text">
+                            <h4 class="icon-box__title">227 Marion Street</h4>
+                            <span class="icon-box__subtitle">Columbia, SC 29201</span></div>
+                    </div>
+                </div>
+                <div class="widget  header-widgets__widget  widget-icon-box">
+                    <div class="icon-box"><i class="fa  fa-clock-o  fa-3x"></i>
+                        <div class="icon-box__text">
+                            <h4 class="icon-box__title">Mon - Sat 8.00 - 18.00</h4>
+                            <span class="icon-box__subtitle">Sunday CLOSED</span></div>
+                    </div>
+                </div> -->
+
+                <div class="widget  header-widgets__widget  widget-social-icons">
+                    <a class="social-icons__link" href="https://www.facebook.com/web3canvas" target="_blank"><i class="fa  fa-facebook"></i></a>
+                    <a class="social-icons__link" href="https://twitter.com/web3canvas" target="_blank"><i class="fa  fa-twitter"></i></a>
+                    <a class="social-icons__link" href="https://www.youtube.com/user/surjithctly" target="_blank"><i class="fa  fa-youtube"></i></a>
+                </div>
+
+                <div class="widget  header-widgets__widget  widget_text">
+                    <div class="textwidget"><a class="btn  btn-danger  btn-rounded" href="tel:18881234567" target="_self"><i class="fa fa-phone"></i> 1-888-123-4567</a></div>
+
+                </div>
+
+            </div>
+        </header>
+    </div>
+
+    <!-- ========  // END HEADER ======== -->
+
+    <!-- ============================
+      CONTAINER
+     ============================ -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12  col-md-9 col-md-push-3" role="main">
+
+                <!-- ============================
+                      SLIDER
+                     ============================ -->
+
+              
+
+                <!-- ========  // SLIDER ======== -->
+
+                <!-- ============================
+                  CONTENT
+                 ============================ -->
+
+                <div class="content-container" style="padding: 30px;">
+<iframe width="790" height="460" src="https://www.youtube.com/embed/uIxnTi4GmCo" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                   
+
+                </div>
+                <!-- // end .content-container -->
+
+                <!-- ========  // END CONTENT ======== -->
+
+                <!-- ============================
+                  LATEST POSTS
+                 ============================ -->
+
+                <div class="latest-posts">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-4">
+                            <div class="latest-post">
+                                <a class="latest-post__thumbnail" href="blog-single.html"><img width="270" height="190" src="assets/images/image-36-270x190.jpg" class="attachment-latest-posts size-latest-posts wp-post-image" alt="MentalPress 21"></a>
+                                <div class="latest-post__categories">
+                                    <ul class="post-categories">
+                                        <li><a href="#" rel="category tag">Teenage</a></li>
+                                    </ul>
+                                </div>
+                                <h4 class="latest-post__title"><a href="#">Looking for help is sign of strength</a></h4>
+                                <p class="latest-post__excerpt"> Individual treatment is often termed as psychotherapy, and is meant to help people with their emotional issues, which can …</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-md-4">
+                            <div class="latest-post">
+                                <a class="latest-post__thumbnail" href="#"><img width="270" height="190" src="assets/images/image-45-270x190.jpg" class="attachment-latest-posts size-latest-posts wp-post-image" alt="MentalPress 13"></a>
+                                <div class="latest-post__categories">
+                                    <ul class="post-categories">
+                                        <li><a href="#" rel="category tag">Self-improvement</a></li>
+                                        <li><a href="#" rel="category tag">Therapy</a></li>
+                                    </ul>
+                                </div>
+                                <h4 class="latest-post__title"><a href="#">Get the most out of your work day</a></h4>
+                                <p class="latest-post__excerpt"> There are many emotional issues that find a corner in our heart, and refuse to die down. With time, these issues can transform …</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-md-4">
+                            <div class="latest-post">
+                                <a class="latest-post__thumbnail" href="#"><img width="270" height="190" src="assets/images/image-57-270x190.jpg" class="attachment-latest-posts size-latest-posts wp-post-image" alt="MentalPress 1"></a>
+                                <div class="latest-post__categories">
+                                    <ul class="post-categories">
+                                        <li><a href="#" rel="category tag">Children Therapy</a></li>
+                                    </ul>
+                                </div>
+                                <h4 class="latest-post__title"><a href="#">Help your child find new friends</a></h4>
+                                <p class="latest-post__excerpt"> Anxiety is something that exists in everyone’s life to a certain extent, and in a way it is medically known to be helpful …</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- // end div[role=main] -->
+
+            <div class="col-xs-12  col-md-3  col-md-pull-9">
+
+                <!-- ============================
+                  SIDEBAR
+                 ============================ -->
+
+                <div class="sidebar">
+                    <nav id="main-navigation" class="main-navigation__container">
+                        <div class="main-navigation__title"> NAVIGATION</div>
+                        <ul id="menu-main-menu" class="main-navigation">
+
+                        <li class="menu-item current-menu-item current_page_item"><a href="#">Home</a></li>
+                        <li class="menu-item current-menu-item current_page_item"><a href="#">Sports</a></li>
+                        <li class="menu-item current-menu-item current_page_item"><a href="#">Politics</a></li>
+                        <li class="menu-item current-menu-item current_page_item"><a href="#">Entertainment</a></li>
+                            <!-- <li class="menu-item menu-item-has-children"><a href="services.html">Services</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item"><a href="services.html">Services List</a></li>
+                                    <li class="menu-item"><a href="couples-therapy.html">Couples Therapy</a></li>
+                                    <li class="menu-item"><a href="depression-treatment.html">Depression Treatment</a></li>
+                                    <li class="menu-item"><a href="individual-treatment.html">Individual Treatment</a></li>
+                                    <li class="menu-item"><a href="children-therapy.html">Therapy for Children</a></li>
+                                    <li class="menu-item"><a href="anxiety-treatment.html">Anxiety Treatment</a></li>
+                                    <li class="menu-item"><a href="post-divorce-recovery.html">Post-divorce Recovery</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item"><a href="about.html">About Me</a></li>
+                            <li class="menu-item"><a href="blog.html">Blog</a></li>
+                            <li class="menu-item menu-item-has-children"><a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item"><a href="index-top-menu.html">index Top Menu</a></li>
+                                    <li class="menu-item"><a href="index-slider.html">Index Large Slider</a></li>
+                                    <li class="menu-item"><a href="index-fullwidth.html">Index Full Width</a></li>
+                                    <li class="menu-item"><a href="index-rtl.html">Index RTL Version</a></li>
+                                    <li class="menu-item"><a href="blog.html">Blog List</a></li>
+                                    <li class="menu-item"><a href="blog-single.html">Blog Single</a></li>
+                                    <li class="menu-item"><a href="shop.html">Shop Listing</a></li>
+                                    <li class="menu-item"><a href="shop-single.html">Shop Single</a></li>
+                                    <li class="menu-item"><a href="shortcodes.html">Shortcodes</a></li>
+                                    <li class="menu-item"><a href="brochure-box.html">Brochure Box</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item"><a href="contact.html">Contact Us</a></li>
+                        </ul> <a class="btn  btn-primary  btn-featured-page" target="_self" href="appointment.html"> Make an Appointment </a> -->
+                    </nav>
+                    
+                </div>
+
+                <!-- ========  // END SIDEBAR ======== -->
+
+            </div>
+        </div>
+        <!-- // end .row -->
+    </div>
+    <!-- // end .container -->
+
+    <div class="footer-gradient"></div>
+    <footer class="footer">
+        <div class="footer-top">
+            <div class="container  footer-top__divider">
+                <div class="row">
+                    <div class="col-xs-12  col-md-4">
+                        <div class="widget  widget_text">
+                            <h6 class="footer-top__headings">A little something about us</h6>
+                            <div class="textwidget">
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p> <a href="#" class="read-more">READ MORE</a></div>
+                        </div>
+                    </div>
+                    <!-- <div class="col-xs-12  col-md-2">
+                        <div class="widget  widget_nav_menu">
+                            <h6 class="footer-top__headings">Navigation</h6>
+                            <div class="menu-footer-menu-container">
+                                <ul id="menu-footer-menu" class="menu">
+                                   <li class="menu-item"><a href="services.html">Services</a></li>
+                                    <li class="menu-item"><a href="about.html">About Me</a></li>
+                                    <li class="menu-item"><a href="blog.html">Blog</a></li>
+                                    <li class="menu-item"><a href="shop.html">Shop</a></li>
+                                    <li class="menu-item"><a href="appointment.html">Make an Appointment</a></li>
+                                    <li class="menu-item"><a href="contact.html">Contact Us</a></li> 
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12  col-md-2">
+                        <div class="widget  widget_nav_menu">
+                            <h6 class="footer-top__headings">Our Services</h6>
+                            <div class="menu-services-container">
+                                <ul id="menu-services" class="menu">
+                                    <li class="menu-item"><a href="depression-treatment.html">Depression</a></li>
+                                    <li class="menu-item"><a href="anxiety-treatment.html">Anxiety</a></li>
+                                    <li class="menu-item">
+                                        <a href="couples-therapy.html">Couples</a>
+                                    </li>
+                                    <li class="menu-item"><a href="services/individual-treatment/">Individual</a></li>
+                                    <li class="menu-item"><a href="post-divorce-recovery.html">Post-divorce</a></li>
+                                    <li class="menu-item"><a href="children-therapy.html">Children</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12  col-md-4">
+                        <div class="widget  widget_text">
+                            <h6 class="footer-top__headings">Buy Now</h6>
+                            <div class="textwidget">
+                                <p>If you come all the way down here, you probably really like our MentalPress theme. To save you all the troubles finding where to buy this theme, we have a solution for that too. Just click the button below.</p>
+                                <br> <a class="btn  btn-primary" href="http://themeforest.net/item/x/XXXXXX?ref=surjithctly" target="_blank">BUY NOW</a></div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="footer-bottom__left"> <img src="assets/images/logo_bw.png" alt="Logo Footer"></div>
+                <div class="footer-bottom__right">
+                    <div class="footer-bottom__payment-methods"> <span>PAYMENT METHODS:</span> &nbsp; &nbsp; &nbsp; <i class="fa  fa-3x  fa-cc-visa"></i> &nbsp; <i class="fa  fa-3x  fa-cc-mastercard"></i> &nbsp; <i class="fa  fa-3x  fa-cc-amex"></i> &nbsp; <i class="fa  fa-3x  fa-cc-paypal"></i></div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- ========== Javascripts ========= -->
+
+    <!-- Jquery -->
+    <script type="text/javascript" src="{{URL::asset('assets/js/jquery.min.js')}}"></script>
+
+    <!-- Bootstrap JS -->
+    <script type="text/javascript" src="{{URL::asset('assets/js/bootstrap.min.js')}}"></script>
+
+    <!-- Picture Fill -->
+    <script type="text/javascript" src="{{URL::asset('assets/js/picturefill.min.js')}}"></script>
+
+    <!-- Main JS -->
+    <script type="text/javascript" src="{{URL::asset('assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
